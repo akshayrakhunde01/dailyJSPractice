1e1bd76cb1723d968a7a0bdc5b47b9d78eeb8418
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-01 23:13:26 +0530

    fixed code

--- factorial.js
+++ factorial.js
@@ -1,2 +1,22 @@
 // find factorial number of given number
+//1st first approach
 
+// function findFactorial(num){
+//     let factorial=1;
+// for(let i=1; i<=num;i++){
+//     factorial*=i
+// }
+// return factorial;
+// }
+
+// 2nd Approach
+
+function findFactorial(num){
+    if(num===0 || num===1){
+        return 1
+    }
+    return num * findFactorial(num-1)
+
+}
+
+console.log(findFactorial(10))
