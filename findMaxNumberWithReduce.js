b728cf71ce5dc36eb805033e9cf92030d94dc107
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-08 23:23:22 +0530

    solved

--- findMaxNumberWithReduce.js
+++ findMaxNumberWithReduce.js
@@ -1,3 +1,13 @@
 // Find the largest element in an array using the 'reduce()` method.
 
-// function 
+function findMaxWithReduce(arr){
+    const findMaxx= arr.reduce((max,curr)=>{
+        return curr>max?curr:max
+    })
+
+    return findMaxx
+}
+
+const array =[1,2,33,4,5,777,88,25,35]
+
+console.log(findMaxWithReduce(array))
