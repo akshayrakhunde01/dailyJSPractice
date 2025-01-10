44ea441769981773266f07ffc013bc1d0be9b9c5
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-10 23:40:45 +0530

    reolced with async await

--- trycatcherro.js
+++ trycatcherro.js
@@ -1,9 +1,17 @@
 // Implement a try-catch block to handle an error that occurs during API data fetching.
 
 async function  fetchData(url){
-    let response = await fetch(url)
+    
 
-    try{}catch(err){
+    try{
+        let response = await fetch(url)
+        let data= await response.json() 
+        console.log(data)
+    }catch(err){
         console.log('error occurd:',err)
     }
 }
+
+let url ='https://jsonplaceholder.typicode.com/posts/1';
+
+console.log(fetchData(url))
