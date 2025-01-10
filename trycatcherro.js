5d70398b2e824d388b0863a3dae9793eaa0bb36e
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-10 23:28:50 +0530

    fixed

--- trycatcherro.js
+++ trycatcherro.js
@@ -1,1 +1,9 @@
 // Implement a try-catch block to handle an error that occurs during API data fetching.
+
+async function  fetchData(url){
+    let response = await fetch(url)
+
+    try{}catch(err){
+        console.log('error occurd:',err)
+    }
+}
