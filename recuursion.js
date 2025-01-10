63239fdd0d442647369cfd6606b6684145141174
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-10 23:48:56 +0530

    resolved

--- recuursion.js
+++ recuursion.js
@@ -1,6 +1,17 @@
 // Recursion 
 // Write a recursive function to calculate the factorial of a given number.
 
-function calculateFactorial(){
-    
+function calculateFactorial(number){
+    number = Number(number)
+    if(number <0 ||number===0 || number===1){
+        return 1
+    }else{
+        
+        return number *(calculateFactorial(number-1))
+    }
 }
+
+
+let number=-1;
+
+console.log(calculateFactorial(number))
