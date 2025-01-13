e39306e42a163e7a2200b992bc1d71bc27113249
akshayrakhunde01@gmail.com <akshayrakhunde01@gmail.com>
2025-01-13 23:42:29 +0530

    resolved

--- increaseCounter.js
+++ increaseCounter.js
@@ -1,3 +1,15 @@
 // *Closures Create a counter function using 
 // closures that increments and returns the count on each call.
 
+function createCounter(){
+    let counter =0;
+
+    return function(){
+        return ++counter 
+    }
+}
+
+let counter = createCounter()
+
+console.log(counter())
+console.log(counter())
